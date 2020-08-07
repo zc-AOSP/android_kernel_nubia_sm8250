693096a222c81e9862135f183b9672989258c9f4
Davide Garberi <dade.garberi@gmail.com>
2020-08-07 15:58:18 +0200

    gpio: Report gameswitch keyevents correctly
    Change-Id: I8bc9668370639546964880ad6e7633912c0b2dcb

--- include/uapi/linux/input-event-codes.h
+++ include/uapi/linux/input-event-codes.h
@@ -337,6 +337,10 @@
 #define KEY_RFKILL		247	/* Key that controls all radios */
 
 #define KEY_MICMUTE		248	/* Mute / unmute the microphone */
+#ifdef CONFIG_NUBIA_KEYBOARD_GAMESWITCH
+#define KEY_GAMESWITCH_ON	250
+#define KEY_GAMESWITCH_OFF	251
+#endif
 
 /* Code 255 is reserved for special needs of AT keyboard driver */
 
@@ -802,6 +806,7 @@
 #define SW_MUTE_DEVICE		0x0e  /* set = device disabled */
 #define SW_PEN_INSERTED		0x0f  /* set = pen inserted */
 #define SW_MACHINE_COVER	0x10  /* set = cover closed */
+#define SW_GAMESWITCH_CHANGE 0x15
 #define SW_MAX			0x10
 #define SW_CNT			(SW_MAX+1)
 
