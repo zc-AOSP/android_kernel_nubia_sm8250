LineageOS/android_kernel_asus_sm8350

techpack: display: dsi_panel: Re-set hbm when turning screen on
Change-Id: I5502a369e934fb60fcf4afc578acdeaf40a1da53

techpack/display/msm/dsi/dsi_panel.c
@@ -4965,6 +4965,10 @@ int dsi_panel_enable(struct dsi_panel *panel)
 		       panel->name, rc);
 	else
 		panel->panel_initialized = true;
+
+	if (panel->manual_hbm_enabled)
+		dsi_panel_set_hbm(panel, true);
+
 	mutex_unlock(&panel->panel_lock);
 	return rc;
 }

zc-AOSP/android_kernel_nubia_sm8250

techpack/display/msm/dsi/dsi_panel.c
@@ -4804,6 +4804,10 @@ int dsi_panel_enable(struct dsi_panel *panel)
 		       panel->name, rc);
 	else
 		panel->panel_initialized = true;
+
+	if (panel->hbm_enabled)
+		dsi_panel_set_hbm(panel, true);
+
 	mutex_unlock(&panel->panel_lock);
 	return rc;
 }